cd3cd94e5ff3a4b943ba8a3eadee794391492009
abhay01kg <64420283+abhay01kg@users.noreply.github.com>
2025-02-27 00:44:45 +0530

    Add files via upload

---
+++ dim_products.sql
@@ -0,0 +1,15 @@
+-- SQL Query to categorize products based on their price
+
+SELECT 
+    ProductID,  
+    ProductName, 
+    Price,  
+
+    CASE -- Categorizes the products into price categories: Low, Medium, or High
+        WHEN Price < 50 THEN 'Low'  
+        WHEN Price BETWEEN 50 AND 200 THEN 'Medium' 
+        ELSE 'High'  
+    END AS PriceCategory 
+
+FROM 
+    dbo.products; 
